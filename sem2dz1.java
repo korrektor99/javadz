2586d1490969cf5c8bf7f2eda9bcb24de26200a0
2 1 <korrektorrussia@mail.ru>
2023-02-13 20:26:17 +0700

    dz1 done

---
+++ sem2dz1.java
@@ -0,0 +1,12 @@
+public class sem2dz1 {
+     public static boolean isPalindrome(String text) {
+        String replaced = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String reversed = new StringBuffer(replaced).reverse().toString();
+        return reversed.equals(replaced);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("hello"));
+    }
+    }
+
