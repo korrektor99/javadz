e2ba2c84fd89876f3065ef0a60f915f3c98d2cd6
2 1 <korrektorrussia@mail.ru>
2023-02-21 23:27:15 +0700

    dz4 done

---
+++ sem4dz1.java
@@ -0,0 +1,79 @@
+// Принимает от пользователя и “запоминает” строки.
+// Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+// Если введено revert, удаляет предыдущую введенную строку из памяти.
+import java.util.*;;
+public class sem4dz1 {
+    public static void main(String[] args) {
+    System.out.println("Введите строки для запоминания");
+    System.out.println("Введите print для вывода запомненного задом-наперед");
+    System.out.println("Введите revert для удаления последней введенной строки");
+    Deque<String> deque = new ArrayDeque<>();
+    String print= "print";
+    String revert = "revert";
+    String exit= "exit";
+    Scanner console = new Scanner(System.in);
+    String result;
+    while (true) {
+        deque.addLast(console.next());
+        result=deque.getLast();
+            if (result.equals(exit))
+            {
+                break;
+            }
+        
+            if (result.equals(print))
+            {
+                deque.removeLast();
+                while(deque.peek()!=null)
+            {
+                System.out.println(deque.removeLast());
+            }
+
+            System.out.println("зашел в 1");
+            }
+            if (result.equals(revert))
+            {
+                deque.removeLast();
+                deque.removeLast();
+                System.out.println("зашел в 2");
+            }
+    
+        
+    }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //deque.add(console.next());
+    //System.out.println(deque.pollLast());
+    // String a= console.next();
+    // if (a=="print")
+    // {
+    //     System.out.println("1");
+    //     System.out.println(deque.pollLast());
+    // }
+    // if (a=="revert")
+    // {
+    //     System.out.println("2");
+    //     deque.removeLast();
+    //     System.out.println("продолжайте");
+    // }
+    // else 
+    // deque.add(console.next());
+    
+}
+
